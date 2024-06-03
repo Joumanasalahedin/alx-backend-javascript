33bcf6d670f831984c2dc9d349362fc08fdcfa8f
Joumana <joumanasalahedin@gmail.com>
2024-06-03 14:39:59 +0200

    task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,7 @@
 export default function handleResponseFromAPI(promise) {
   return promise
     .then(() => ({
-      status: 200, body: 'sucess',
+      status: 200, body: 'success',
     }
     ))
     .catch(() => Error())
