d6213357e048aaeb655059668d0f1d1677ef37c4
Joumana <joumanasalahedin@gmail.com>
2024-06-03 15:19:13 +0200

    task 6

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,18 @@
+import { signUpUser } from './4-user-promise';
+import { uploadPhoto } from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, filename) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
+    .then((results) => results.map((result) => {
+      if (result.status === 'fulfilled') {
+        return {
+          status: result.status,
+          value: result.value,
+        };
+      }
+      return {
+        status: result.status,
+        value: result.reason.message,
+      };
+    }));
+}
