3f309bb6cff6afdfaa64c2d539f4400c3d6ba082
Joumana <joumanasalahedin@gmail.com>
2024-06-03 15:43:48 +0200

    task 9

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -6,7 +6,7 @@ export default function guardrail(mathFunction) {
   } catch (error) {
     queue.push(`Error: ${error.message}`);
   } finally {
-    queue.push('Guardraile was processed');
+    queue.push('Guardrail was processed');
   }
 
   return queue;
