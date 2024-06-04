0c75dbef922e19d50ff28f0618da97b488ec2ef1
Joumana <joumanasalahedin@gmail.com>
2024-06-04 23:15:57 +0200

    task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,11 @@
 export default function cleanSet(set, startString) {
-  if (!startString || !set) {
+  if (!startString || !set || !(set instanceof Set) || typeof startString !== 'string') {
     return '';
   }
   const result = [];
   for (const value of set) {
-    if (value.startsWith(startString)) {
-      result.push(value.slice(startString.length));
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      result.push(value.substring(startString.length));
     }
   }
   return result.join('-');
