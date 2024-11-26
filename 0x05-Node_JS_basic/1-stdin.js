eab4fa86374756522f89c70dc1f5c98b27546573
Joumana Elsayed <joumanasalahedin@gmail.com>
2024-11-26 22:53:25 +0100

    switch out console.log

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,8 @@
-console.log('Welcome to Holberton School, what is your name?');
-
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.setEncoding('utf-8');
-
 process.stdin.on('data', (input) => {
   const name = input.trim();
-  console.log(`Your name is: ${name}`);
-
-  console.log('This important software is now closing');
+  process.stdout.write(`Your name is: ${name}\n`);
+  process.stdout.write('This important software is now closing\n');
   process.exit();
 });
